46d525f8ea80ace3f83cbfa64510bffb5ef85644
Brock Wilcox <awwaiid@thelackthereof.org>
2012-02-16 01:46:09 +0000

    Import of AWWAIID/Audio-NoiseGen-0.01 from CPAN.
    gitpan-cpan-distribution: Audio-NoiseGen
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         AWWAIID/Audio-NoiseGen-0.01.tar.gz
    gitpan-cpan-author:       AWWAIID
    gitpan-cpan-maturity:     released

---
+++ eg/mary.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/env perl
+
+use strict;
+use Audio::NoiseGen ':all';
+Audio::NoiseGen::init();
+
+play(
+  envelope_gen(
+    { attack => 0.2, sustain => 14.5, decay => 0.2 },
+    combine_gen(
+      segment_gen('
+        E D C D
+        E E E R
+        D D D R
+        E E E R
+        E D C D
+        E E E/2 E
+        D D E D C
+      '),
+      segment_gen('A2 R R R'),
+      segment_gen('C3/2 E3/4 E3/4 C3/2 F3 R'),
+    ),
+  )
+, 'out.raw');
+
